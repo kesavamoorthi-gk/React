81e014a89aba994e41649183e4a54b0e395e65bd
kesavamoorthi-gk <kesavamoorthi@sparkouttech.com>
2023-11-30 10:59:45 +0530

    Secind Project

---
+++ Image-Carousal/src/App.tsx
@@ -0,0 +1,13 @@
+import Buttons from "./components/Buttons";
+import Image from "./components/Image";
+
+const App = () => {
+  return (
+    <>
+      <Image />;
+      <Buttons />
+    </>
+  );
+};
+
+export default App;
---
+++ Image-Carousal/src/components/Image.tsx
@@ -0,0 +1,18 @@
+const Image = () => {
+  return (
+    <>
+      <img
+        className="img-fluid"
+        src="https://media.istockphoto.com/id/1457289049/photo/search-engine-optimization-seo-data-searching-technology-business-hands-typing-on-laptop.jpg?s=1024x1024&w=is&k=20&c=BhmTpnJ4j5liLFxqlDhsdWOqk_bQJ3vhm-7MU_UaySg="
+        alt=""
+      />
+      <img
+        className="img-fluid"
+        src="https://media.istockphoto.com/id/1491277691/photo/closeup-of-business-woman-using-mobile-smart-phone-and-working-on-laptop-computer-on-office.jpg?s=1024x1024&w=is&k=20&c=And9fo0_W8YgnqyNOLbmlXWZdF4VZ_g3BAOYwP0pQdc="
+        alt=""
+      />
+    </>
+  );
+};
+
+export default Image;
